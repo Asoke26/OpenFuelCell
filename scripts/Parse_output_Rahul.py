df44ee9266e65f75621f464ec5a4742da12b3142
RAHUL <rahulhoskeri7@gmail.com>
2019-11-01 15:42:59 -0700

    Update Parse_output_Rahul.py

--- scripts/Parse_output_Rahul.py
+++ scripts/Parse_output_Rahul.py
@@ -2,10 +2,10 @@
 raw_input = "raw-input-output.txt"
 f=open(raw_input,"r")
 
-out_file = "out.txt"
+out_file = "out.csv"
 w_f = open(out_file,'w+')
 
-line = "Time,Tair_min,Tair_mean,Tair_max,Tfuel_min,Tfuel_mean,Tfuel_max,rhoAir_min,rhoAir_mean,rhoAir_max,rhoFuel_min,rhoFuel_mean,rhoFuel_max,muAir_min,muAir_mean,muAir_max,muFuel_min,muFuel_mean,muFuel_max,nuAir_min,nuAir_mean,nuAir_max,nuFuel_min,nuFuel_mean,nuFuel_max,kAir_min,kAir_mean,kAir_max,kFuel_min,kFuel_mean,kFuel_max,layer_no,sumVolume,Nern_min,Nern_max,ibar0,ibar,V,stack_Voltage,min_curr,mean_curr,max_curr,Energy_Ir,Energy_Fr,Energy_t_min,Energy_t_mean,Energy_t_max"
+line = "Time,Tair_min,Tair_mean,Tair_max,Tfuel_min,Tfuel_mean,Tfuel_max,rhoAir_min,rhoAir_mean,rhoAir_max,rhoFuel_min,rhoFuel_mean,rhoFuel_max,muAir_min,muAir_mean,muAir_max,muFuel_min,muFuel_mean,muFuel_max,nuAir_min,nuAir_mean,nuAir_max,nuFuel_min,nuFuel_mean,nuFuel_max,kAir_min,kAir_mean,kAir_max,kFuel_min,kFuel_mean,kFuel_max,sumVolume,Nern_min,Nern_max,ibar0,ibar,V,stack_Voltage,min_curr,mean_curr,max_curr,Energy_Ir,Energy_Fr,Energy_t_min,Energy_t_mean,Energy_t_max"
 w_f.writelines(line)
 #w_f.writelines("\n")
 s=[]
@@ -80,12 +80,19 @@ for i in f:
         s.append(str(c[2]))
         s.append(str(c[8]))
         s.append(str(c[14]).rstrip("\n"))
-        #print(s)
-##Energy values##
-    if "Solving for T, Initial residual" in i:
+    if "stack Voltage =" in i:
         c=i.split(" ")
-        s.append(str(c[8]))
-        s.append(str(c[12]))
+        s.append(c[2][1:-1])
+    if "min,mean,max(current): " in i:
+        c=i.split(" ")
+        s.append(c[1])
+        s.append(c[3])
+        s.append(c[5].rstrip("\n"))
+##Energy values##
+    if "Solving for T, Initial residual = " in i:
+        c=i.split(" ")
+        s.append(str(c[8][:-1]))
+        s.append(str(c[12][:-1]))
     if "T min mean max     =" in i:
         c=i.split(" ")
         s.append(str(c[9]))
